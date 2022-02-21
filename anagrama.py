96f784abccb6bf0ad498bd7499dcaa15bae46010
JhefferVini <jeffervinicius@hotmail.com>
2022-02-20 22:18:09 -0300

    Resolução dos desafios da Capgemini

---
+++ anagrama.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def count_anagrams(string):
+    buckets = {}
+    for i in range(len(string)):
+        for j in range(1, len(string) - i + 1):
+            key = frozenset(Counter(string[i:i+j]).items()) # O(N) time key extract
+            buckets[key] = buckets.get(key, 0) + 1
+    count = 0
+    for key in buckets:
+        count += buckets[key] * (buckets[key]-1) // 2
+    return count
+
+x = count_anagrams(input('Digite uma palavra: '))
+print(x)
---
+++ escadinha.py
@@ -0,0 +1,6 @@
+def escadinha(name):
+    for i in range(len(name)+1):
+        print(name[:i])
+
+escadinha(input('Digite apenas "*": '))
+
